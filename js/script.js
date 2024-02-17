155a624f71e858346b35d5c0a11765151427c5da
Abdur Rob <136497573+Abdurrobr34l@users.noreply.github.com>
2024-02-17 18:09:34 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// javascript
+
+// IT IS USED TO GIVE GO TO TOP BUTTON
+
+// get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+// jquery
+$(document).ready(function () {
+
+// IT IS USED TO OFF TOGGLER WHEN LINK IS CLICKED
+$('.js-scroll-trigger').click(function () {
+  $('.navbar-collapse').collapse('hide');
+});
+
+
+});
